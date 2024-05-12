08ac9cce62a55de36f539a40e4d82e07326967f5
Madhav Adakula <adakula.madhav@worldline.com>
2024-05-12 08:40:47 +0530

    main

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@RequestMapping("/test")
+	public String helloWorld() {
+		System.out.println("in helloWorld");
+		
+		
+		return "Hello spring boot";
+	}
+	
+	
+}
